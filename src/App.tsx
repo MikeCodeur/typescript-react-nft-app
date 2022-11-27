bbe56799f27b11c96cb4459647794e9366137062
Mike Codeur <cabanas.mickael@gmail.com>
2022-11-27 18:28:31 +0800

    prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,87 @@
-export * from './App.exercise'
+import React from 'react'
+import './css/styles.css'
 
-//export * from './App.final'
+import {Card} from './components/Card/Card'
+import {Search} from './components/Search/Search'
+import {Footer} from './components/Footer/Footer'
+import {MainContainer} from './components/Containers/MainContainer'
+import {CardsContainer} from './components/Containers/CardsContainer'
+import {nftType} from './types/types'
 
-//export * from './App.bonus-1'
+type JSONResponse = {
+  data?: Array<Omit<nftType, 'fetchedAt' | 'createdAt'>>
+  elements?: number
+  errors?: Array<{message: string}>
+}
 
-//export * from './App.bonus-2'
+function App() {
+  const [nftsApi, setNftsApi] = React.useState<nftType[] | undefined>([])
+  const [nfts, setNfts] = React.useState<nftType[] | undefined>([])
+  const [errors, setErrors] = React.useState<
+    Array<{message: string}> | undefined
+  >([])
+  const [elements, setElements] = React.useState<number>()
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    const value = e.target.value
+    const filteredList = nftsApi?.filter(nft => {
+      return (
+        nft.title.toLowerCase()?.includes(value.toLowerCase()) ||
+        nft.description.toLowerCase().includes(value.toLowerCase()) ||
+        nft.creator.toLowerCase().includes(value.toLowerCase())
+      )
+    })
+    setNfts(filteredList)
+    setElements(filteredList?.length)
+  }
+
+  React.useEffect(() => {
+    const api = async () => {
+      const resp = await fetch(
+        'https://636b20b6c07d8f936dae7fe4.mockapi.io/api/nft/all',
+        {
+          method: 'GET',
+        },
+      )
+      const {data, errors, elements}: JSONResponse = await resp.json()
+      if (resp.ok) {
+        console.log(data)
+        setNftsApi(data)
+        setNfts(data)
+        setErrors(errors)
+        setElements(elements)
+      } else {
+        setErrors([{message: `API en erreur : code ${resp.status}`}])
+        setNftsApi([])
+        setNfts([])
+        setElements(0)
+      }
+    }
+    api()
+    return () => {}
+  }, [])
+
+  return (
+    <MainContainer>
+      <Search onChange={handleChange} />
+      {elements ?? -1 > 0 ? (
+        <div role={'alert'} className="elements">
+          il y a {elements} element(s)
+        </div>
+      ) : null}
+      {errors?.length ?? -1 > 0 ? (
+        <div role={'alert'} className="alert">
+          il y a {errors?.length} erreur(s) : {errors?.[0].message}
+        </div>
+      ) : null}
+      <CardsContainer>
+        {nfts?.map((nft, idx) => (
+          <Card key={idx} nft={nft} />
+        ))}
+      </CardsContainer>
+      <Footer />
+    </MainContainer>
+  )
+}
+
+export {App}
